ced4b3f3cce9cdb51b5e42568b48cf6d89d656fe
roconnorr <roryoconnor@outlook.co.nz>
2020-03-28 15:53:51 +1300

    Remove draggable flatlist, add a regular flatlist implementation for now

--- App.tsx
+++ App.tsx
@@ -1,63 +1,131 @@
-import React, { Component, ReactElement } from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
-import DraggableFlatList from 'react-native-draggable-flatlist';
+import React, { ReactElement } from 'react';
+import { View, StyleSheet, Text, FlatList, TouchableOpacity } from 'react-native';
 
-const SEED_DATA = [
-  {
-    title: 'Now',
-    data: ['Pizza', 'Burger', 'Risotto', 'Cheese Cake', 'Ice Cream'],
-  },
-  {
-    title: 'Later',
-    data: ['French Fries', 'Onion Rings', 'Fried Shrimps', 'Water', 'Coke', 'Beer'],
-  },
-];
+const SEED_DATA = {
+  now: ['Pizza', 'Burger', 'Risotto', 'Cheese Cake', 'Ice Cream'],
+  later: ['French Fries', 'Onion Rings', 'Fried Shrimps', 'Water', 'Coke', 'Beer'],
+};
 
-const exampleData = [...Array(20)].map((d, index) => ({
-  key: `item-${index}`, // For example only -- don't use index as your key!
-  label: index,
+type ListData = {
+  key: string;
+  name: string;
+  list: 'now' | 'later';
+  backgroundColor: string;
+  disabledDrag?: boolean;
+  disabledReSorted?: boolean;
+};
+
+const nowData: ListData[] = SEED_DATA.now.map((item, index) => ({
+  name: item,
+  key: item,
+  list: 'now',
   backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${index * 5}, ${132})`,
 }));
 
-class App extends Component {
-  state = {
-    data: exampleData,
+const laterData: ListData[] = SEED_DATA.later.map((item, index) => ({
+  name: item,
+  key: item,
+  list: 'later',
+  backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${index * 5}, ${132})`,
+}));
+
+interface State {
+  nowData: ListData[];
+  laterData: ListData[];
+}
+
+class App extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      nowData,
+      laterData,
+    };
+  }
+
+  onPressItem = (item: ListData): void => {
+    if (item.list === 'now') {
+      const now = this.state.nowData;
+
+      const nowIndex = now.findIndex((i) => i.name === item.name);
+      now.splice(nowIndex, 1);
+
+      this.setState({
+        nowData: now,
+        laterData: [...this.state.laterData, { ...item, list: 'later' }],
+      });
+    } else if (item.list === 'later') {
+      const later = this.state.laterData;
+
+      const laterIndex = later.findIndex((i) => i.name === item.name);
+      later.splice(laterIndex, 1);
+
+      this.setState({
+        nowData: [...this.state.nowData, { ...item, list: 'now' }],
+        laterData: later,
+      });
+    }
   };
 
-  renderItem = ({ item, index, drag, isActive }): ReactElement => {
+  renderItem(item: ListData): ReactElement {
     return (
       <TouchableOpacity
-        style={{
-          height: 100,
-          backgroundColor: isActive ? 'blue' : item.backgroundColor,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-        onLongPress={drag}>
-        <Text
-          style={{
-            fontWeight: 'bold',
-            color: 'white',
-            fontSize: 32,
-          }}>
-          {item.label}
-        </Text>
+        style={[styles.item, { backgroundColor: item.backgroundColor }]}
+        key={item.key}
+        onPress={(): void => this.onPressItem(item)}>
+        <Text style={styles.itemText}>{item.name}</Text>
       </TouchableOpacity>
     );
-  };
+  }
 
   render(): ReactElement {
     return (
-      <View style={{ flex: 1 }}>
-        <DraggableFlatList
-          data={this.state.data}
-          renderItem={this.renderItem}
-          keyExtractor={(item, index) => `draggable-item-${item.key}`}
-          onDragEnd={({ data }) => this.setState({ data })}
+      <View style={{ flex: 1, paddingTop: 50 }}>
+        <Text>Now</Text>
+        <FlatList
+          data={this.state.nowData}
+          renderItem={({ item }): ReactElement => this.renderItem(item)}
+          scrollEnabled={false}
+          numColumns={4}
+          style={{ width: '100%' }}
+        />
+
+        <Text>Later</Text>
+        <FlatList
+          data={this.state.laterData}
+          renderItem={({ item }): ReactElement => this.renderItem(item)}
+          scrollEnabled={false}
+          numColumns={4}
+          style={{ width: '100%' }}
         />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  MainContainer: {
+    justifyContent: 'center',
+    flex: 1,
+    paddingTop: 30,
+  },
+  button: {
+    width: 150,
+    height: 100,
+    backgroundColor: 'blue',
+  },
+  item: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  itemText: {
+    fontSize: 20,
+    color: '#FFFFFF',
+  },
+});
 
 export default App;

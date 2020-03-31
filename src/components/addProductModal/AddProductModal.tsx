6e4604c18cfdf40e6f8926c7ba85daae049fa0c1
roconnorr <roryoconnor@outlook.co.nz>
2020-03-31 17:31:03 +1300

    Reset text field after adding a product

--- src/components/addProductModal/AddProductModal.tsx
+++ src/components/addProductModal/AddProductModal.tsx
@@ -35,7 +35,7 @@ const AddProductModal = (): ReactElement => {
                   quantity: 1,
                 })
               );
-              console.log(store.getState());
+              setNewProductName('');
               setModalVisible(false);
             }}
             icon={(style: ImageStyle): ReactElement => (
